0bba1e6762ad30f43c6f4f11e91ec60a2de1617f
blackjack <576843117@qq.com>
2019-05-20 00:10:34 +0800

    get face data

--- shininglight/app.py
+++ shininglight/app.py
@@ -1,18 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,json,request
 
 app = Flask(__name__)
+
+app.config['JSON_AS_ASCII'] = False
+
 
 @app.route('/')
 def index():
     # this action go to face detection page
     return render_template('face.html')
 
-@app.route('/store')
+@app.route('/store',methods=['POST'])
 def store_face():
     # store face
-    pass
+    return request.form['img']
 
-@app.route('/search')
+@app.route('/search',methods=['POST'])
 def search_face():
     # search face in storage
     pass
